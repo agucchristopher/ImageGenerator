7ba3c8556560b6bfb7c8fe096ac2b96c23fe899c
Agu C Christopher <aguchris740@gmail.com>
2023-01-20 12:58:46 +0100

    Final Code

--- js/index.js
+++ js/index.js
@@ -2,60 +2,34 @@ async function generateImageRequest(prompt, size) {
   try {
     showSpinner();
 
-    // let headersList = {
-    //   Accept: "*/*",
-    //   "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-    //   "Content-Type": "application/x-www-form-urlencoded",
-    // };
-
-    // let bodyContent = `prompt=${prompt}`;
-
-    // let response = await fetch(
-    //   "http://chatgpt-image-api.vercel.app/ai/generateimage",
-    //   {
-    //     method: "POST",
-    //     body: bodyContent,
-    //     mode: "no-cors",
-    //     headers: {
-    //       Accept: "*/*",
-    //       "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-    //       "Content-Type": "application/x-www-form-urlencoded",
-    //     },
-    //   }
-    // );
-
     let headersList = {
       Accept: "*/*",
       "User-Agent": "Thunder Client (https://www.thunderclient.com)",
       "Content-Type": "application/x-www-form-urlencoded",
-      "Access-Control-Allow-Origin": "*",
     };
 
     let bodyContent = `prompt=${prompt}`;
 
     let response = await fetch(
-      "http://chatgpt-image-api.vercel.app/ai/generateimage",
+      "http://openai-nodejs-api.vercel.app/ai/createimage",
 
       {
         method: "POST",
         body: bodyContent,
         headers: headersList,
-        mode: "no-cors",
       }
     );
 
-    let data = await response;
+    let data = await response.json();
     console.log("data", data);
 
-    // if (!response.ok) {
-    //   removeSpinner();
-    //   throw new Error("That image could not be generated");
-    // }
+    if (!response.ok) {
+      removeSpinner();
+      throw new Error("That image could not be generated");
+    }
 
-    // const data = await response.json();
-    // console.log(data);
-
-    const imageUrl = data.data;
+    const imageUrl = data.image;
+    console.log("img", imageUrl);
 
     document.querySelector(".image").src = imageUrl;
 
