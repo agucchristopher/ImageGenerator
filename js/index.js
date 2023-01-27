a8c641a04666020bc280b56e65cb22852776f639
Agu C. Christopher <aguchris740@gmail.com>
2023-01-27 11:15:47 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const generateImageRequest = async (prompt, size) => {
   try {
     showSpinner();
-    alert('Function Called') 
+  //  alert('Function Called') 
     let headersList = {
       Accept: "*/*",
       "User-Agent": "Thunder Client (https://www.thunderclient.com)",
